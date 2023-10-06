7dd70c0173ef58a1203d97404a41f4de7c107fab
Marilisk <6868221@gmail.com>
2023-10-06 16:54:31 +0300

    key

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 export const API_URL = 'https://secondapi.spboptis.ru:3443'
 
 
-const API_SECRET = import.meta.env.VITE_API_SECRET
+//const API_SECRET = import.meta.env.VITE_API_SECRET
 
 const instance = axios.create({  
     baseURL: API_URL, 
@@ -14,7 +14,7 @@ const instance = axios.create({
 })
 
 instance.interceptors.request.use( (config) => {
-    config.headers['authorization'] = `${API_SECRET}`
+    config.headers['authorization'] = "45913410-5c7f-11ee-a132-d9aecc67d3a6"
     return config;
 })
 
